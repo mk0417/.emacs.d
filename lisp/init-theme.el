dd6e863f339eca2449cdad6efa2898233f03c397
“mk0417” <pengdata1@gmail.com>
2022-02-23 22:51:45 +0000

    upstream changes

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -4,16 +4,16 @@
 (straight-use-package 'modus-themes)
 
 ;; modus
-(setq modus-themes-bold-constructs t
-      modus-themes-italic-constructs t
-      modus-themes-links '(faint bold background)
-      modus-themes-prompts '(bold background)
-      modus-themes-mode-line '(moody)
-      modus-themes-completions 'opinionated
-      modus-themes-fringes 'intense
-      modus-themes-hl-line '(intense)
-      modus-themes-paren-match '(intense bold underline)
-      modus-themes-region '(no-extend accented))
+(setq modus-themes-bold-constructs t)
+(setq modus-themes-italic-constructs t)
+(setq modus-themes-links '(faint bold background))
+(setq modus-themes-prompts '(bold background))
+(setq modus-themes-mode-line '(moody))
+(setq modus-themes-fringes 'intense)
+(setq modus-themes-hl-line '(intense))
+(setq modus-themes-paren-match '(intense bold underline))
+(setq modus-themes-region '(no-extend accented))
+(setq modus-themes-completions '((t background intense accented)))
 
 (setq modus-themes-org-agenda
       '((header-block . (variable-pitch scale-title))
