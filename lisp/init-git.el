d33737431948e7bd1d1caacb06773abc97af35e3
Peng <pengdotli@gmail.com>
2022-06-18 22:21:11 +0100

    use margin

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -19,7 +19,8 @@
 
 (dolist (hook '(prog-mode-hook conf-mode-hook markdown-mode-hook))
   (add-hook hook (lambda ()
-                   (diff-hl-mode))))
+                   (diff-hl-mode)
+                   (diff-hl-margin-mode))))
 
 ;;; git-messenger
 (setq git-messenger:show-detail t)
