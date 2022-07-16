300680275c2a214730b8bfdad458cb75adc06ebc
Peng <pengdotli@gmail.com>
2022-07-16 20:51:37 +0100

    add winum

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -1,5 +1,11 @@
 ;;;;; init-windows.el --- Windows config -*- lexical-binding: t -*-
 
+;;; Install packages
+(straight-use-package 'winum)
+
+;;; winum
+(add-hook 'after-init-hook 'winum-mode)
+
 ;;; Split new buffer on the right by default
 (setq split-height-threshold nil)
 
@@ -9,6 +15,10 @@
     :prefix "SPC"
     :states '(normal visual))
   (p-space-leader-def
+    "SPC" 'execute-extended-command
+    "1" '(winum-select-window-1 :which-key "select window 1")
+    "2" '(winum-select-window-2 :which-key "select window 2")
+    "3" '(winum-select-window-3 :which-key "select window 3")
     "w" '(:ignore t :which-key "window")
     "wl" '(evil-window-right :which-key "move to right window")
     "wh" '(evil-window-left :which-key "move to left window")
