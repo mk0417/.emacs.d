cc8c525c34f1dff019bbdc420deabc3d11d46bb1
Peng <pengdotli@gmail.com>
2022-07-16 20:52:02 +0100

    add denote

--- init.el
+++ init.el
@@ -56,6 +56,7 @@
 (require 'init-avy)
 (require 'init-org)
 (require 'init-template)
+(require 'init-notes)
 
 ;;; Make GC pauses faster by decreasing the threshold
 (setq gc-cons-threshold (* 2 1000 1000))
---
+++ lisp/init-notes.el
@@ -0,0 +1,48 @@
+;;;;; init-notes.el --- Note -*- lexical-binding: t -*-
+
+;;; Install packages
+(straight-use-package 'denote)
+
+;;; Denote
+(require 'denote)
+
+(setq denote-directory (expand-file-name "~/Dropbox/peng_notes/"))
+(setq denote-known-keywords '("emacs" "python"))
+(setq denote-infer-keywords t)
+(setq denote-sort-keywords t)
+(setq denote-file-type nil)
+(setq denote-prompts '(title subdirectory))
+(setq denote-allow-multi-word-keywords t)
+(setq denote-date-format nil)
+(setq denote-link-fontify-backlinks t)
+
+;; (require 'denote-retrieve)
+;; (require 'denote-link)
+
+;; If you use Markdown or plain text files (Org renders links as buttons right away)
+(add-hook 'find-file-hook #'denote-link-buttonize-buffer)
+
+(defun p-denote-journal ()
+  (interactive)
+  (denote
+   (denote--title-prompt)
+   '("journal")))
+
+;;; Keybindings
+(with-eval-after-load 'evil
+  (general-create-definer p-space-leader-def
+    :prefix "SPC"
+    :states '(normal visual))
+  (p-space-leader-def
+    "SPC" 'execute-extended-command
+    "n" '(:ignore t :which-key "notes")
+    "nn" '(denote :which-key "denote")
+    "nj" '(denote-subdirectory :which-key "denote subdirectory")
+    "nl" '(denote-link :which-key "denote link")
+    "nL" '(denote-link-add-links :which-key "denote links")
+    "nv" '(denote-link-find-file :which-key "denote find file")
+    "ns" '(denote-link-backlinks :which-key "denote backlinks")
+    "no" '(p-denote-journal :which-key "denote journal")))
+
+(provide 'init-notes)
+;;;;; init-notes.el ends here
