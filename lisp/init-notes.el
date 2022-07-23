dbd8b7b657ef0e56db98a5d711f539f8414cab99
Peng <pengdotli@gmail.com>
2022-07-23 13:39:11 +0100

    update

--- lisp/init-notes.el
+++ lisp/init-notes.el
@@ -22,6 +22,12 @@
 ;; If you use Markdown or plain text files (Org renders links as buttons right away)
 (add-hook 'find-file-hook #'denote-link-buttonize-buffer)
 
+;; Find file in my notes
+(defun p-find-file-in-notes ()
+  (interactive)
+  (let ((default-directory (file-truename (file-name-directory (expand-file-name "~/Dropbox/peng_notes/")))))
+    (call-interactively 'find-file)))
+
 (defun p-denote-journal ()
   (interactive)
   (denote
@@ -40,9 +46,11 @@
     "nj" '(denote-subdirectory :which-key "denote subdirectory")
     "nl" '(denote-link :which-key "denote link")
     "nL" '(denote-link-add-links :which-key "denote links")
-    "nv" '(denote-link-find-file :which-key "denote find file")
+    "nv" '(denote-link-find-file :which-key "denote link find file")
     "ns" '(denote-link-backlinks :which-key "denote backlinks")
-    "no" '(p-denote-journal :which-key "denote journal")))
+    "no" '(p-denote-journal :which-key "denote journal")
+    "f" '(:ignore t :which-key "file")
+    "fn" '(p-find-file-in-notes :which-key "find notes")))
 
 (provide 'init-notes)
 ;;;;; init-notes.el ends here
