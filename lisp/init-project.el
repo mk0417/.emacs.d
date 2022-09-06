8c172678c081d6ea5d16de91af73f9892e1f407d
Peng <pengdotli@gmail.com>
2022-09-06 22:27:34 +0100

    add project find func

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -11,6 +11,14 @@
          (list (cons (straight--repos-dir) 1))))
     (magit-list-repositories)))
 
+(defun p-project-find-regexp ()
+  (interactive)
+  (defvar xref-show-xrefs-function)
+  (let ((xref-show-xrefs-function #'consult-xref))
+    (if-let ((tap (thing-at-point 'symbol)))
+        (project-find-regexp tap)
+      (call-interactively #'project-find-regexp))))
+
 ;;; Keybindings
 (with-eval-after-load 'evil
   (general-create-definer p-space-leader-def
@@ -25,7 +33,9 @@
     "pr" '(straight-remove-unused-repos :which-key "straight-remove-unused-repos")
     "pU" '(straight-pull-recipe-repositories :which-key "straight-pull-recipe-repositories")
     "pu" '(straight-pull-all  :which-key "straight update all packages")
-    "pS" '(p-list-installed-packages :which-key "list installed packages")))
+    "pS" '(p-list-installed-packages :which-key "list installed packages")
+    "s"  '(:ignore t :which-key "search")
+    "sd" '(p-project-find-regexp :which-key "project find regexp")))
 
 (provide 'init-project)
 ;;;;; init-project.el ends here
