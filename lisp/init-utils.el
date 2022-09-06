00596e0827d1b01bb3a552739c91afbc49add499
Peng <pengdotli@gmail.com>
2022-09-06 22:28:20 +0100

    delete search dictionary func

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -59,15 +59,6 @@
                            (buffer-substring (region-beginning) (region-end))
                          (read-string "Search YouTube: "))))))
 
-(defun p-dictionary-search ()
-  (interactive)
-  (browse-url
-   (concat
-    "https://dictionary.cambridge.org/dictionary/english/"
-    (url-hexify-string (if mark-active
-                           (buffer-substring (region-beginning) (region-end))
-                         (read-string "Search Dictionary: "))))))
-
 ;;; Insert date
 ;; http://xahlee.info/emacs/emacs/elisp_insert-date-time.html
 (defun p-choose-and-insert-date ()
@@ -124,7 +115,6 @@
     "s"  '(:ignore t :which-key "search")
     "sg" '(p-google-search :which-key "search on google")
     "sy" '(p-youtube-search :which-key "search on youtube")
-    "sd" '(p-dictionary-search :which-key "search dictionary")
     "t"  '(:ignore t :which-key "toggle")
     "tr" '(p-reveal-file-in-finder :which-key "reveal file in finder")))
 
