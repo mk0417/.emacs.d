403d78e20a9f88f89da7abb12cb84b359d722134
Peng <pengdotli@gmail.com>
2022-09-22 11:23:45 +0100

    config modeline

---
+++ lisp/init-modeline.el
@@ -0,0 +1,25 @@
+;;;;; init-modeline.el --- Modeline -*- lexical-binding: t -*-
+
+;; https://gitlab.com/protesilaos/dotfiles/-/blob/master/emacs/.emacs.d/prot-emacs-modules/prot-emacs-modeline.el
+(setq mode-line-percent-position '(-3 "%p"))
+(setq mode-line-position-column-line-format '(" %l,%c"))
+(setq mode-line-defining-kbd-macro (propertize " Macro" 'face 'mode-line-emphasis))
+
+(setq-default mode-line-format
+              '("%e"
+                mode-line-front-space
+                mode-line-mule-info
+                mode-line-client
+                mode-line-modified
+                mode-line-remote
+                mode-line-frame-identification
+                mode-line-buffer-identification
+                "  "
+                mode-line-position
+                mode-line-modes
+                "  "
+                (vc-mode vc-mode)
+                "  "
+                mode-line-end-spaces))
+
+(provide 'init-modeline)
