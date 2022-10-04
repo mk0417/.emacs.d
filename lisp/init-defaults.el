9b596183b7308a609353dd60ea5b2d03e1dbeafe
Peng <pengdotli@gmail.com>
2022-10-04 13:22:25 +0100

    enable repeat-mode

--- lisp/init-defaults.el
+++ lisp/init-defaults.el
@@ -80,6 +80,9 @@
 ;;; Keep small kill ring
 (setq kill-ring-max 60)
 
+;;; Enable repeat-mode
+(repeat-mode 1)
+
 ;;; Hide "setting up indent for shell type zsh"
 ;; https://emacs.stackexchange.com/questions/52846/how-to-remove-message-indentation-setup-for-shell-type-sh
 (advice-add 'sh-set-shell :around
