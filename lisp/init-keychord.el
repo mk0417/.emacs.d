8b054a7a6aaea6cd7b2e6a1f29be048f86873a4b
Peng <pengdotli@gmail.com>
2023-03-28 22:41:35 +0100

    update

--- lisp/init-keychord.el
+++ lisp/init-keychord.el
@@ -33,7 +33,7 @@
     (key-chord-define mode ";a" "<-")
     (key-chord-define mode ";j" "%>%")
     (key-chord-define mode ";m" "%*%")
-    (key-chord-define mode ";p" "%%")
+    (key-chord-define mode ";p" "%%\C-b")
     (key-chord-define mode ";f" "5")
     (key-chord-define mode ";i" "6")
     (key-chord-define mode ";s" "7")
