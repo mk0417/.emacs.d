a24e4d88f96c5e64fcdbe3116e9fc156cbf5f749
Peng <pengdotli@gmail.com>
2023-07-04 12:08:52 +0100

    update vertico

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -17,19 +17,92 @@
 (setq vertico-cycle t)
 
 (vertico-mode 1)
-(vertico-multiform-mode)
 
 ;; sort directories first
-(defun sort-directories-first (files)
-  (setq files (vertico-sort-history-alpha files))
-  (nconc (seq-filter (lambda (x) (string-suffix-p "/" x)) files)
-         (seq-remove (lambda (x) (string-suffix-p "/" x)) files)))
+;; (defun sort-directories-first (files)
+;;   (setq files (vertico-sort-history-alpha files))
+;;   (nconc (seq-filter (lambda (x) (string-suffix-p "/" x)) files)
+;;          (seq-remove (lambda (x) (string-suffix-p "/" x)) files)))
+
+;; (setq vertico-multiform-categories
+;;       '((symbol (vertico-sort-function . vertico-sort-alpha))
+;;         (file (vertico-sort-function . sort-directories-first))
+;;         (t (vertico-sort-function . vertico-sort-history-alpha))))
+
+(defvar prot-vertico-minimal
+  '(unobtrusive
+    (vertico-flat-format . ( :multiple  ""
+                             :single    ""
+                             :prompt    ""
+                             :separator ""
+                             :ellipsis  ""
+                             :no-match  "")))
+  "List of configurations for minimal Vertico multiform.
+The minimal view is intended to be more private or less
+revealing.  This is important when, for example, a prompt shows
+names of people.  Of course, such a view also provides a minimal
+style for general usage.
+
+Toggle the vertical view with the `vertico-multiform-vertical'
+command or use the commands `prot-vertico-private-next' and
+`prot-vertico-private-previous', which toggle the vertical view
+automatically.")
+
+(defvar prot-vertico-maximal
+  '((vertico-count . 10)
+    (vertico-resize . t))
+  "List of configurations for maximal Vertico multiform.")
 
 (setq vertico-multiform-categories
-      '((symbol (vertico-sort-function . vertico-sort-alpha))
-        (file (vertico-sort-function . sort-directories-first))
-        (t (vertico-sort-function . vertico-sort-history-alpha))))
+      `(;; Maximal
+        (embark-keybinding ,@prot-vertico-maximal)
+        (multi-category ,@prot-vertico-maximal)
+        (consult-location ,@prot-vertico-maximal)
+        (imenu ,@prot-vertico-maximal)
+        (unicode-name ,@prot-vertico-maximal)
+        ;; Minimal
+        (file ,@prot-vertico-minimal (vertico-preselect . prompt))
+        (t ,@prot-vertico-minimal)))
 
+(vertico-multiform-mode 1)
+
+(defun prot-vertico-private-next ()
+  "Like `vertico-next' but toggle vertical view if needed.
+This is done to accommodate `prot-vertico-minimal'."
+  (interactive)
+  (if vertico-unobtrusive-mode
+      (let ((vertico--index 0))
+        (vertico-multiform-vertical)
+        (vertico-next 1))
+    (vertico-next 1)))
+
+(defun prot-vertico-private-previous ()
+  "Like `vertico-previous' but toggle vertical view if needed.
+This is done to accommodate `prot-vertico-minimal'."
+  (interactive)
+  (if vertico-unobtrusive-mode
+      (progn
+        (vertico-multiform-vertical)
+        (vertico-previous 1))
+    (vertico-previous 1)))
+
+(prot-emacs-keybind vertico-map
+  "DEL" #'vertico-directory-delete-char
+  "M-DEL" #'vertico-directory-delete-word
+  "M-," #'vertico-quick-insert
+  "M-." #'vertico-quick-exit)
+
+(prot-emacs-keybind vertico-multiform-map
+  "C-n" #'prot-vertico-private-next
+  "<down>" #'prot-vertico-private-next
+  "C-p" #'prot-vertico-private-previous
+  "<up>" #'prot-vertico-private-previous
+  "C-l" #'vertico-multiform-vertical)
+
+;; This works with `file-name-shadow-mode' enabled.  When you are in
+;; a sub-directory and use, say, `find-file' to go to your home '~/'
+;; or root '/' directory, Vertico will clear the old path to keep
+;; only your current input.
 (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy)
 
 (define-key vertico-map (kbd "M-h") 'vertico-directory-up)
