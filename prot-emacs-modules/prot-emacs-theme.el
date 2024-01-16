53107b429dcacd697d6838c59402b9b21155a113
Peng <pengdotli@gmail.com>
2024-01-16 16:26:35 +0000

    update

--- prot-emacs-modules/prot-emacs-theme.el
+++ prot-emacs-modules/prot-emacs-theme.el
@@ -192,7 +192,6 @@
            :inherit medium
            :default-height 150)
           (presentation
-           :inherit medium
            :default-weight light
            :default-height 180)
           (t
