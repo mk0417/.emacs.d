3548596dcda1754d1df54290efeeac9846591bd1
Peng <pengdotli@gmail.com>
2024-03-28 20:46:14 +0000

    update

--- lisp/init-keychord.el
+++ lisp/init-keychord.el
@@ -16,14 +16,17 @@
 This should normally be a little longer than `key-chord-two-keys-delay'."
   :type 'float)
 
-(defcustom key-chord-safety-interval-backward 0
-  "Min time to distinguish a key chord and preceding inputs."
-  :type 'float)
+(defcustom key-chord-in-macros t
+  "If nil, don't expand key chords when executing keyboard macros.
 
-(defcustom key-chord-safety-interval-forward 0
-  "Min time delay to distinguish a key chord and following inputs."
-  :type 'float)
+If non-nil, expand chord sequenses in macros, but only if a
+similar chord was entered during the last interactive macro
+recording. (This carries a bit of guesswork. We can't know for
+sure when executing whether two keys were typed quickly or slowly
+when recorded.)"
+  :type 'boolean)
 
+;; Internal vars
 (defvar key-chord-mode nil)
 
 ;; Shortcut for key-chord-input-method: no need to test a key again if it
@@ -36,35 +39,74 @@ This should normally be a little longer than `key-chord-two-keys-delay'."
 (defvar key-chord-in-last-kbd-macro nil)
 (defvar key-chord-defining-kbd-macro nil)
 
-;; Internal vars for
-;; `key-chord-safety-interval-backward'. `key-chord-idle-state' is
-;; non-nil iff at least `key-chord-safety-interval-backward' past
-;; after the last (non-keychord) input.
-(defvar key-chord-idle-state t)
-(defvar key-chord-timer-object nil)
-
 ;;;###autoload
 (define-minor-mode key-chord-mode
   "Map pairs of simultaneously pressed keys to commands.
 
-See `key-chord-define' and variables `key-chord-two-keys-delay'
+See functions `key-chord-define-global', `key-chord-define-local',
+and `key-chord-define' and variables `key-chord-two-keys-delay'
 and `key-chord-one-key-delay'."
   :global t
-  (if key-chord-mode
-      (progn
-        (setq input-method-function 'key-chord-input-method)
-        (setq key-chord-timer-object
-              (run-with-idle-timer
-               key-chord-safety-interval-backward 'repeat
-               (lambda () (setq key-chord-idle-state t)))))
-    (cancel-timer key-chord-timer-object)
-    (setq input-method-function nil)
-    (setq key-chord-timer-object nil)))
+  (setq input-method-function
+        (and key-chord-mode
+             'key-chord-input-method)))
+
+;;;###autoload
+(defun key-chord-define-global (keys command)
+  "Define a key-chord of the two keys in KEYS starting a COMMAND.
+
+KEYS can be a string or a vector of two elements. Currently only
+elements that corresponds to ascii codes in the range 32 to 126
+can be used.
+
+COMMAND can be an interactive function, a string, or nil.
+If COMMAND is nil, the key-chord is removed.
+
+Note that KEYS defined locally in the current buffer will have
+precedence."
+  (interactive "sSet key chord globally (2 keys): \nCSet chord \"%s\" to command: ")
+  (key-chord-define (current-global-map) keys command))
+
+;;;###autoload
+(defun key-chord-define-local (keys command)
+  "Locally define a key-chord of the two keys in KEYS starting a COMMAND.
+
+KEYS can be a string or a vector of two elements. Currently only
+elements that corresponds to ascii codes in the range 32 to 126
+can be used.
+
+COMMAND can be an interactive function, a string, or nil.
+If COMMAND is nil, the key-chord is removed.
+
+The binding goes in the current buffer's local map, which in most
+cases is shared with all other buffers in the same major mode."
+  (interactive "sSet key chord locally (2 keys): \nCSet chord \"%s\" to command: ")
+  (key-chord-define (current-local-map) keys command))
+
+(defun key-chord-unset-global (keys)
+  "Remove global key-chord of the two keys in KEYS."
+  (interactive "sUnset key chord globally (2 keys): ")
+  (key-chord-define (current-global-map) keys nil))
+
+(defun key-chord-unset-local (keys)
+  "Remove local key-chord of the two keys in KEYS."
+  (interactive "sUnset key chord locally (2 keys): ")
+  (key-chord-define (current-local-map) keys nil))
 
 ;;;###autoload
 (defun key-chord-define (keymap keys command)
+  "Define in KEYMAP, a key-chord of the two keys in KEYS starting a COMMAND.
+
+KEYS can be a string or a vector of two elements. Currently only
+elements that corresponds to ascii codes in the range 32 to 126
+can be used.
+
+COMMAND can be an interactive function, a string, or nil.
+If COMMAND is nil, the key-chord is removed."
   (if (/= 2 (length keys))
       (error "Key-chord keys must have two elements"))
+  ;; Exotic chars in a string are >255 but define-key wants 128..255
+  ;; for those.
   (let ((key1 (logand 255 (aref keys 0)))
         (key2 (logand 255 (aref keys 1))))
     (if (eq key1 key2)
@@ -72,11 +114,13 @@ and `key-chord-one-key-delay'."
       (define-key keymap (vector 'key-chord key1 key2) command))))
 
 (defun key-chord-lookup-key1 (keymap key)
+  "Like lookup-key but no third arg and no numeric return value."
   (let ((res (lookup-key keymap key)))
     (and (not (numberp res))
          res)))
 
 (defun key-chord-lookup-key (key)
+  "Lookup KEY in all current key maps."
   (let ((maps (current-minor-mode-maps))
         res)
     (while (and maps (not res))
@@ -87,10 +131,18 @@ and `key-chord-one-key-delay'."
              (key-chord-lookup-key1 (current-local-map) key))
         (key-chord-lookup-key1 (current-global-map) key))))
 
+(defun key-chord-describe ()
+  "List key chord bindings in a help buffer.
+
+Two key chords will be listed twice and there will be Prefix
+Commands. Please ignore that."
+  (interactive)
+  (describe-bindings [key-chord]))
+
 (defun key-chord-input-method (first-char)
+  "Input method controlled by key bindings with the prefix `key-chord'."
   (cond
-   ((and key-chord-idle-state
-         (not (eq first-char key-chord-last-unmatched))
+   ((and (not (eq first-char key-chord-last-unmatched))
          (key-chord-lookup-key (vector 'key-chord first-char)))
     (let ((delay (if (key-chord-lookup-key
                       (vector 'key-chord first-char first-char))
@@ -102,14 +154,12 @@ and `key-chord-one-key-delay'."
                  (eldoc-pre-command-refresh-echo-area))
                (sit-for delay 'no-redisplay))
              (setq key-chord-last-unmatched nil)
-             (setq key-chord-idle-state nil)
              (list first-char))
             (t ; input-pending-p
              (let* ((input-method-function nil)
                     (next-char (read-event))
                     (res (vector 'key-chord first-char next-char)))
-               (cond ((and (key-chord-lookup-key res)
-                           (sit-for key-chord-safety-interval-forward 'no-redisplay))
+               (cond ((key-chord-lookup-key res)
                       (setq key-chord-defining-kbd-macro
                             (cons first-char key-chord-defining-kbd-macro))
                       (list 'key-chord first-char next-char))
@@ -118,12 +168,18 @@ and `key-chord-one-key-delay'."
                             (cons next-char unread-command-events))
                       (when (eq first-char next-char)
                         (setq key-chord-last-unmatched first-char))
-                      (setq key-chord-idle-state nil)
                       (list first-char))))))))
    (t ; no key-chord keymap
     (setq key-chord-last-unmatched first-char)
-    (setq key-chord-idle-state nil)
     (list first-char))))
+
+(defun key-chord--start-kbd-macro (_append &optional _no-exec)
+  (setq key-chord-defining-kbd-macro nil))
+(advice-add 'start-kbd-macro :after #'key-chord--start-kbd-macro)
+
+(defun key-chord--end-kbd-macro (&optional _repeat _loopfunc)
+  (setq key-chord-in-last-kbd-macro key-chord-defining-kbd-macro))
+(advice-add 'end-kbd-macro :after #'key-chord--end-kbd-macro)
 
 (key-chord-mode 1)
 
@@ -135,7 +191,7 @@ and `key-chord-one-key-delay'."
     (key-chord-define mode "aa" "&")
     (key-chord-define mode "qq" "@")
     (key-chord-define mode "vv" "#")
-    (key-chord-define mode "gt" "!")
+    (key-chord-define mode "jt" "!")
     (key-chord-define mode "zz" "$")
     (key-chord-define mode "zl" "|")
     (key-chord-define mode "fh" "%")
@@ -153,7 +209,7 @@ and `key-chord-one-key-delay'."
     (key-chord-define mode ";h" "<>\C-b")
     (key-chord-define mode ";a" "<-")
     (key-chord-define mode ";j" "%>%")
-    (key-chord-define mode ";p" "|>")
+    (key-chord-define mode ";l" "|>")
     (key-chord-define mode ";m" "%*%")
     (key-chord-define mode ";g" "=>")
     (key-chord-define mode ";k" "->")
