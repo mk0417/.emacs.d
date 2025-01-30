f7d727dc182473fa5e8fa861b7022796516a39f0
Peng <pengdotli@gmail.com>
2025-01-30 10:20:00 +0000

    update

--- prot-emacs-modules/prot-emacs-org.el
+++ prot-emacs-modules/prot-emacs-org.el
@@ -78,6 +78,7 @@
     ("s" . org-narrow-to-subtree)
     :map ctl-x-x-map
     ("i" . prot-org-id-headlines)
+    ("r" . prot-org-id-headlines-readable)
     ("h" . prot-org-ox-html))
   :config
   ;; My custom extras, which I use for the agenda and a few other Org features.
