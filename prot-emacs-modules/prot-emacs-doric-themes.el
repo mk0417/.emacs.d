d0a3b86932d6a31e82ec7a5e8fd1ca73abd6a42d
Peng <pengdotli@gmail.com>
2025-05-23 08:18:55 +0100

    update

--- prot-emacs-modules/prot-emacs-doric-themes.el
+++ prot-emacs-modules/prot-emacs-doric-themes.el
@@ -9,14 +9,7 @@
   (setq doric-themes-to-toggle '(doric-light doric-dark))
   (setq doric-themes-to-rotate doric-themes-collection)
 
-  (with-eval-after-load 'lin
-    (setq lin-face 'hl-line)
-    (lin-global-mode -1)
-    (global-hl-line-mode 1))
-
-  (global-display-line-numbers-mode 1)
-
-  (doric-themes-load-random
+(doric-themes-load-random
    (if (prot-emacs-theme-environment-dark-p)
        'dark
      'light))
@@ -25,3 +18,5 @@
   (("<f5>" . doric-themes-toggle)
    ("C-<f5>" . doric-themes-select)
    ("M-<f5>" . doric-themes-rotate)))
+
+(provide 'prot-emacs-doric-themes)
