3f344dadfc72d289d2b1f71dd918243a203796f1
Peng <pengdotli@gmail.com>
2025-07-28 08:13:00 +0100

    update

--- prot-emacs-modules/prot-emacs-theme.el
+++ prot-emacs-modules/prot-emacs-theme.el
@@ -67,11 +67,11 @@
   :init
   ;; These are the defaults, but I keep it here for visiibility.
   (setq spacious-padding-widths
-        `( :internal-border-width 12
+        `( :internal-border-width 15
            :header-line-width 2
            :mode-line-width 1
            :tab-width 4
-           :right-divider-width 20
+           :right-divider-width 15
            :scroll-bar-width ,(if x-toolkit-scroll-bars 8 6)
            :left-fringe-width 3
            :right-fringe-width 3))
