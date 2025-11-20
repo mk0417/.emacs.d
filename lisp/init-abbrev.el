d27cd44f0cece0c644b7bf2f55cde036e749019b
Peng <pengdotli@gmail.com>
2025-11-20 20:17:18 +0000

    update

--- lisp/init-abbrev.el
+++ lisp/init-abbrev.el
@@ -1,8 +1,16 @@
 ;;; init-abbrev.el --- Abbreviations -*- lexical-binding: t -*-
 
-(define-abbrev global-abbrev-table
-  "mywrds"
-  "def wrds_connection():
+(progn
+  (when (boundp 'python-mode-abbrev-table)
+    (clear-abbrev-table python-mode-abbrev-table))
+  (define-abbrev-table 'python-mode-abbrev-table
+    '(
+      ("mywrds"
+       "with open(Path('~/.pass.yml').expanduser()) as f:
+    wrds_username, wrds_password = [*yaml.safe_load(f)['wrds'].values()]")
+
+      ("mywrdsconn"
+       "def wrds_connection():
     with open(Path('~/.pass.yml').expanduser()) as f:
         wrds_username, wrds_password = [*yaml.safe_load(f)['wrds'].values()]
 
@@ -10,5 +18,8 @@
         f'postgresql://{wrds_username}:{wrds_password}'
         '@wrds-pgdata.wharton.upenn.edu:9737/wrds?sslmode=require')
     return uri")
+      )))
+
+(setq save-abbrevs nil)
 
 (provide 'init-abbrev)
