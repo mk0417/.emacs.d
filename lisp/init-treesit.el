9bffb6451a9a2f8530e0e885ab5afddb91e18821
Peng <pengdotli@gmail.com>
2025-11-28 15:15:28 +0000

    update

--- lisp/init-treesit.el
+++ lisp/init-treesit.el
@@ -7,12 +7,19 @@
         (json . ("https://github.com/tree-sitter/tree-sitter-json"))
         (markdown . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" nil "tree-sitter-markdown/src"))
         (markdown-inline . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" nil "tree-sitter-markdown-inline/src"))
-        (org . ("https://github.com/milisims/tree-sitter-org"))
         (python . ("https://github.com/tree-sitter/tree-sitter-python"))
-        (sql . ("https://github.com/m-novikov/tree-sitter-sql"))
         (toml . ("https://github.com/ikatyang/tree-sitter-toml"))
         (typst . ("https://github.com/uben0/tree-sitter-typst"))
         (yaml . ("https://github.com/ikatyang/tree-sitter-yaml"))))
+
+(setq major-mode-remap-alist
+      '((conf-toml-mode . toml-ts-mode)
+        (css-mode . css-ts-mode)
+        (json-mode . json-ts-mode)
+        (markdown-mode . markdown-ts-mode)
+        (python-mode . python-ts-mode)
+        (sh-mode . bash-ts-mode)
+        (yaml-mode . yaml-ts-mode)))
 
 (defun p-treesit-install-all-languages ()
   (interactive)
@@ -21,9 +28,6 @@
 	  (treesit-install-language-grammar lang)
 	  (message "`%s' parser was installed." lang)
 	  (sit-for 1))))
-
-(add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-ts-mode))
-(add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
 
 (provide 'init-treesit)
 
