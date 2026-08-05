b5a54cc21fe377ea86dcbf390c6777788f8d06f3
Peng <pengdotli@gmail.com>
2026-08-05 18:15:45 +0100

    update

--- prot-emacs-modules/prot-emacs-window.el
+++ prot-emacs-modules/prot-emacs-window.el
@@ -279,4 +279,40 @@ With optional argument FRAME, return the list of buffers of FRAME."
 ;;   ;; The use the command `buffer-to-pdf' or `buffer-to-pdf-black-on-white'.
 ;;   (prot-emacs-install buffer-to-pdf "https://github.com/protesilaos/buffer-to-pdf.git"))
 
+(prot-emacs-configure
+  (setq speedbar-use-images nil) ; t here refers to the `cdr' of each `speedbar-expand-image-button-alist'
+  (setq speedbar-smart-directory-expand-flag nil)
+  (setq speedbar-prefer-window t ; both of these are for Emacs 31
+        speedbar-window-max-width 60)
+  (setq speedbar-indentation-width 4)
+  (setq speedbar-show-unknown-files t)
+  ;; NOTE 2026-08-05 17:08 +0300: Waiting for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=81556 which I just submitted.
+  (setq speedbar-directory-unshown-regexp nil
+        speedbar-file-unshown-regexp nil)
+  (prot-emacs-hook speedbar-visiting-tag-hook (speedbar-recenter speedbar-highlight-one-tag-line))
+
+  (defun prot/speedbar-imenu-flatten (elements)
+    "Flatten the Imenu ELEMENTS."
+    (let* ((elements (imenu--flatten-index-alist elements t))
+           (elements-no-prefix (mapcar
+                                (lambda (element)
+                                  (when-let* ((string (car element))
+                                              (position (cdr element))
+                                              (_ (or (natnump position) (markerp position))))
+                                    (cond
+                                     ((string-match-p ":.*:" string)
+                                      (cons
+                                       (string-join (cdr (split-string string "[:]")))
+                                       position))
+                                     ((string-match-p ":" string)
+                                      (cons
+                                       (cadr (split-string string "[:]"))
+                                       position))
+                                     (t
+                                      element))))
+                                elements)))
+      (delq nil elements-no-prefix)))
+
+  (setq speedbar-tag-hierarchy-method '(prot/speedbar-imenu-flatten)))
+
 (provide 'prot-emacs-window)
