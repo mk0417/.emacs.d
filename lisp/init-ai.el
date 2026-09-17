6074c58c310fc03a32f2bbd579a10d0e515b4e14
Peng <pengdotli@gmail.com>
2026-09-17 12:14:18 +0100

    update

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -3,6 +3,37 @@
 (use-package gptel
   :ensure t
   :config
+  (defun p-gpt-rewrite-english ()
+    (interactive)
+    (require 'gptel)
+    (unless (use-region-p)
+      (user-error "Please select a region of text to rewrite"))
+    (let* ((start (region-beginning))
+           (end (region-end))
+           (text (buffer-substring-no-properties start end))
+           (prompt
+            (format
+             "Rewrite and improve the following English text.
+              Return ONLY the rewritten text.
+              Do not explain your changes.
+              Do not include reasoning.
+              Do not use quotes around the result.
+             Text:
+              %s"
+             text)))
+      (setq gptel-model "inclusionai/ling-3.0-flash-vl:free")
+      (gptel-request
+          prompt
+        :callback
+        (lambda (response _metadata)
+          (when (stringp response)
+            (let ((response (string-trim response)))
+              (unless (string-empty-p response)
+                (save-excursion
+                  (goto-char start)
+                  (delete-region start end)
+                  (insert response)))))))))
+
   (setq gptel-include-reasoning nil)
   (setq open-router-key (with-temp-buffer
                           (insert-file-contents "~/.ai_key/openrouter.txt")
@@ -15,7 +46,6 @@
           :endpoint "/api/v1/chat/completions"
           :stream t
           :key open-router-key
-          :models '(openrouter/hunter-alpha
-                    qwen/qwen3-coder:free))))
+          :models '(inclusionai/ling-3.0-flash-vl:free))))
 
 (provide 'init-ai)
